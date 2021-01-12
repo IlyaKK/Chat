8e79a5472a44dadef44f1b41a7b1d3f465c7bcf5
IlyaK <ilja.kasatckin@yandex.ru>
2021-01-12 17:08:49 +0300

    Выполнил основную часть задания, решил добавить ещё окно для пользователей

---
+++ src/sample/Controller.java
@@ -0,0 +1,63 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class Controller {
+    @FXML
+    private ListView<String> listPerson;
+
+    @FXML
+    private ListView<String> listMessage;
+
+    @FXML
+    private TextField messageField;
+
+    @FXML
+    private Button sendBtn;
+
+    private final ObservableList<String> messageList = FXCollections.observableArrayList("привет!", "hello", "hey");
+
+    private final ObservableList<String> personList = FXCollections.observableArrayList("Марк", "Коля", "Настя");
+
+    @FXML
+    public void initialize(){
+        listMessage.setItems(messageList);
+        listPerson.setItems(personList);
+    }
+
+    @FXML
+    public void sendMessage() {
+        String message = messageField.getText();
+        if(!message.isBlank()){
+            addMessageToListMessage(message);
+        }else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Ошибка");
+            alert.setHeaderText("Ошибка ввода данных");
+            alert.setContentText("Не отправляйте пустое сообщение");
+            alert.show();
+        }
+        messageField.clear();
+    }
+
+    private void addMessageToListMessage(String message) {
+        listMessage.getItems().add(message);
+    }
+
+    @FXML
+    void exit() {
+        System.exit(0);
+    }
+
+    @FXML
+    void showAbout() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("About");
+        alert.setHeaderText("Homework 4");
+        alert.setContentText("Chat");
+        alert.show();
+    }
+}
